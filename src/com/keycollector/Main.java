8b1f9548522aeceb8a0a1421b9770b8f8592f170
theflybirdstudio <ken_gaming@hotmail.com>
2017-08-14 19:58:47 +0800

    Reformatted to original code.

--- src/com/keycollector/Main.java
+++ src/com/keycollector/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println(33);
-        System.out.println(25);
-        System.out.println(332);
     }
 
 }
