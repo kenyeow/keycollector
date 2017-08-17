d0463a8c7f9d0f17695ea9b85c7ab4566b8b3990
theflybirdstudio <ken_gaming@hotmail.com>
2017-08-17 22:36:22 +0800

    Added new methods for encapsulation.

--- src/com/keycollector/GameImage.java
+++ src/com/keycollector/GameImage.java
@@ -1,11 +1,7 @@
 package com.keycollector;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
-import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Creted by Yeow Wan Shung
@@ -14,32 +10,36 @@ import java.util.List;
 
 public class GameImage {
 
-    final ImageIcon banGei;
-    final ImageIcon arkImides;
-    final ImageIcon canSer;
-    final ImageIcon dozCiztem;
-
-    final ImageIcon pinkey;
-    final ImageIcon donkey;
-    final ImageIcon keyDisk;
-    final ImageIcon keyNote;
-    final ImageIcon monkey;
-
-    final ImageIcon treasureChest;
+    private final ImageIcon treasureChest;
+    private ArrayList<ImageIcon> playerIcons;
+    private ArrayList<ImageIcon> keyIcons;
 
     public GameImage(){
-        banGei = new ImageIcon("icons/1.gif");
-        arkImides = new ImageIcon("icons/2.gif");
-        canSer = new ImageIcon("icons/3.gif");
-        dozCiztem = new ImageIcon("icons/4.gif");
+        playerIcons = new ArrayList<>();
+        playerIcons.add(new ImageIcon("icons/1.gif"));
+        playerIcons.add(new ImageIcon("icons/2.gif"));
+        playerIcons.add(new ImageIcon("icons/3.gif"));
+        playerIcons.add(new ImageIcon("icons/4.gif"));
 
-        pinkey = new ImageIcon("icons/a.gif");
-        donkey = new ImageIcon("icons/b.gif");
-        keyDisk = new ImageIcon("icons/c.gif");
-        keyNote = new ImageIcon("icons/d.gif");
-        monkey = new ImageIcon("icons/e.gif");
+        keyIcons = new ArrayList<>();
+        keyIcons.add(new ImageIcon("icons/a.gif"));
+        keyIcons.add(new ImageIcon("icons/b.gif"));
+        keyIcons.add(new ImageIcon("icons/c.gif"));
+        keyIcons.add(new ImageIcon("icons/d.gif"));
+        keyIcons.add(new ImageIcon("icons/e.gif"));
 
         treasureChest = new ImageIcon("icons/5.gif");
     }
 
+    public ImageIcon getPlayerIcon(int position){
+        return playerIcons.get(position);
+    }
+
+    public ImageIcon getKeyIcon(int position){
+        return keyIcons.get(position);
+    }
+
+    public ImageIcon getTreasureChest() {
+        return treasureChest;
+    }
 }
