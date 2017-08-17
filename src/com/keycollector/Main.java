cf4869e7830a1ac8d899315c07e23d87b07b0626
theflybirdstudio <ken_gaming@hotmail.com>
2017-08-17 22:37:31 +0800

    Removed unwanted import

--- src/com/keycollector/Main.java
+++ src/com/keycollector/Main.java
@@ -1,13 +1,10 @@
 package com.keycollector;
 
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
         KeyCollectorBoard keyCollectorBoard = new KeyCollectorBoard();
-
     }
 
 }
