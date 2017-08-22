b33d6b2204ccd95d7eec7a58e93ff0bfc62f7cdf
theflybirdstudio <ken_gaming@hotmail.com>
2017-08-22 12:20:57 +0800

    Moved (Random) out of method.

--- src/com/keycollector/RandomNumberUtils.java
+++ src/com/keycollector/RandomNumberUtils.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ * Created by Yeow Wan Shung
  * A util class to generate random number with specified range and
  *  add random number with limited range to an array list
  * **/
 public final class RandomNumberUtils {
 
+    private final static Random random = new Random();
+
     public static int generateRandomNumber(int range){
-        Random random = new Random();
         return random.nextInt(range) + 1;
     }
 
