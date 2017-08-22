3d663884c7f9cc6578bc4eeda53c45104543c487
theflybirdstudio <ken_gaming@hotmail.com>
2017-08-22 12:20:13 +0800

    Added Square Class (extends JButton)

---
+++ src/com/keycollector/Squares.java
@@ -0,0 +1,14 @@
+package com.keycollector;
+
+import javax.swing.*;
+
+/**
+ * Created by Yeow Wan Shung
+ * **/
+
+public class Squares extends JButton {
+
+    public Squares(){
+        super();
+    }
+}
