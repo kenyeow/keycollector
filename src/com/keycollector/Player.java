16c0665850a03b887dcfed433a725168ab75cba0
theflybirdstudio <ken_gaming@hotmail.com>
2017-09-07 02:06:25 +0800

    Added player class

---
+++ src/com/keycollector/Player.java
@@ -0,0 +1,33 @@
+package com.keycollector;
+
+import javax.swing.*;
+
+public class Player{
+
+    private ImageIcon icon;
+    private int currentRow;
+    private int currentColumn;
+
+    public Player(ImageIcon icon, int row, int column){
+        this.icon = icon;
+        this.currentRow = row;
+        this.currentColumn = column;
+    }
+
+    public ImageIcon getIcon(){
+        return icon;
+    }
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    public int getCurrentColumn() {
+        return currentColumn;
+    }
+
+    public void setPostion(int row, int coloumn){
+        currentRow = row;
+        currentColumn = coloumn;
+    }
+}
