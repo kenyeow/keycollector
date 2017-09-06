9badf6970c5b043e2b355fc4648a440bd4061757
theflybirdstudio <ken_gaming@hotmail.com>
2017-09-07 02:05:39 +0800

    Gameplay class to start the program.

--- src/com/keycollector/Main.java
+++ src/com/keycollector/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
-        KeyCollectorBoard keyCollectorBoard = new KeyCollectorBoard();
+        Gameplay gameplay = new Gameplay();
     }
 
 }
