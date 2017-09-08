40c81c100bafc8189c926d443688410afc661067
XuanD <xuandeng1029@gmail.com>
2017-09-08 12:38:51 +0800

    Added MoveRestriction and nextPlayer function

--- src/com/keycollector/Gameplay.java
+++ src/com/keycollector/Gameplay.java
@@ -2,38 +2,54 @@ package com.keycollector;
 
 public class Gameplay {
 
-    private KeyCollectorBoard keyCollectorBoard;
-    private static Player player1, player2, player3, player4;
+    private static int x = 0;
+    private static KeyCollectorBoard keyCollectorBoard;
+    private static Player[] player = new Player[4];
     private static Player currentPlayer = null;
     private static Square[][] squares;
+
 
     public Gameplay(){
         GameImage gameImage = new GameImage();
         keyCollectorBoard = new KeyCollectorBoard();
-        player1 = new Player(gameImage.getPlayerIcon(0), 0, 0);
-        player2 = new Player(gameImage.getPlayerIcon(1), 0, 8);
-        player3 = new Player(gameImage.getPlayerIcon(2), 8, 0);
-        player4 = new Player(gameImage.getPlayerIcon(3), 8, 8);
+        player[0] = new Player(gameImage.getPlayerIcon(0), 0, 0);
+        player[1] = new Player(gameImage.getPlayerIcon(1), 0, 8);
+        player[2] = new Player(gameImage.getPlayerIcon(2), 8, 0);
+        player[3] = new Player(gameImage.getPlayerIcon(3), 8, 8);
 
         squares = KeyCollectorBoard.getSquares();
-        squares[0][0].setPlayer(player1);
-        squares[0][8].setPlayer(player2);
-        squares[8][0].setPlayer(player3);
-        squares[8][8].setPlayer(player4);
+        squares[0][0].setPlayer(player[0]);
+        squares[0][8].setPlayer(player[1]);
+        squares[8][0].setPlayer(player[2]);
+        squares[8][8].setPlayer(player[3]);
 
         initializeGamePlay();
     }
 
     private void initializeGamePlay(){
         keyCollectorBoard.setToolBarInfo("Player 1 turn");
-        currentPlayer = player1;
+        currentPlayer = player[0];
     }
 
     public static Player getCurrentPlayer(){
         return currentPlayer;
     }
 
+    public static Player nextPlayer(){
+        x++;
+        if(x > 3)
+        {
+            x = 0;
+        }
+        currentPlayer = player[x];
+        keyCollectorBoard.setToolBarInfo("Player " + x + " turn");
+        return currentPlayer;
+    }
+
+
+
     public static void removePlayerFromSquare(int row, int column){
         squares[row][column].removePlayer();
     }
 }
+
--- src/com/keycollector/Movement.java
+++ src/com/keycollector/Movement.java
@@ -2,30 +2,65 @@ package com.keycollector;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.SynchronousQueue;
 
 public class Movement implements ActionListener {
 
     Player currentPlayer;
+    private int row , column;
+    private int oriRow, oriColumn;
+    private Square squareClicked;
 
-    //TODO: Add Restriction
     private boolean moveRestriction(){
-        return false;
+
+        if(row > oriRow + 2 || row < oriRow - 2)
+        {
+            return false;
+        }
+        else
+        {
+            if(column > oriColumn + 2 || column < oriColumn - 2)
+            {
+                return false;
+            }
+            else
+            {
+                if(squareClicked.isPlayerOccupy() == false)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Square squareClicked = (Square) e.getSource();
-        int row = Integer.parseInt(squareClicked.getClientProperty("row").toString());
-        int column = Integer.parseInt(squareClicked.getClientProperty("column").toString());
+        squareClicked = (Square) e.getSource();
+        row = Integer.parseInt(squareClicked.getClientProperty("row").toString());
+        column = Integer.parseInt(squareClicked.getClientProperty("column").toString());
         System.out.println("Row " + row);
         System.out.println("Column " + column);
 
         currentPlayer = Gameplay.getCurrentPlayer();
-        int oriRow = currentPlayer.getCurrentRow();
-        int oriColumn = currentPlayer.getCurrentColumn();
-        Gameplay.removePlayerFromSquare(oriRow, oriColumn);
+        oriRow = currentPlayer.getCurrentRow();
+        oriColumn = currentPlayer.getCurrentColumn();
+        if(moveRestriction())
+        {
+            Gameplay.removePlayerFromSquare(oriRow, oriColumn);
+            currentPlayer.setPostion(row, column);
+            squareClicked.setPlayer(currentPlayer);
+            currentPlayer = Gameplay.nextPlayer();
+        }
+        else
+        {
+            System.out.println("Invalid Movement");
+        }
 
-        currentPlayer.setPostion(row, column);
-        squareClicked.setPlayer(currentPlayer);
+
     }
 }
